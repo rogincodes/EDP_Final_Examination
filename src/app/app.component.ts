5bf17faf8058ed801ea7cce72a223a796170d6c1
Rogin Cartalla <tetsudrew@gmail.com>
2022-01-07 11:19:25 +0800

    added all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+
+  public no1!:number;
+  public no2!:number;
+  public result!:number;
+
+  allClear() {
+    this.no1 = Number({});
+    this.no2 = Number({});
+    this.result = Number({});
+  }
+
+  add() {
+    this.result = this.no1+this.no2;
+  }
+
+  subtract() {
+    this.result = this.no1-this.no2;
+  }
+  
+  multiply() {
+    this.result = this.no1*this.no2;
+  }
+
+  divide() {
+    this.result = this.no1/this.no2;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
